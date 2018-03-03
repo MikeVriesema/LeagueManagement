176e7bbb3321131008b2c3d5a2ae4f2a4175f3d9
Rian Brady <31370883+RBrady98@users.noreply.github.com>
2018-03-03 22:27:29 +0000

    Update LeagueManagement.java

--- LeagueManagement.java
+++ LeagueManagement.java
@@ -1,15 +1,35 @@
-public class LeagueManagement
+import javax.swing.JOptionPane;
+import java.io.*;
+import java.util.ArrayList;
+
+public class LeagueCreate 
 {
-  public static void main(String args[]) throws IOException
-  {
-   
-    pull request change test
-   
-    String blueJ = blueJ;
-  if(blueJ == blueJ)
-  {
-    System.out.println("blueJ");
-    System.out.println("nameJeff");
-  }
-  }
+    public static void main(String[] args) throws IOException 
+    {
+      
+    }
+
+    public static void createTeams()
+    {
+        ArrayList<String> participants = new ArrayList<String>();
+        boolean nextTeam = true;
+        while(nextTeam == true)
+        {
+            String input = JOptionPane.showInputDialog(null, "Enter team name");
+
+            if(input == null)
+                nextTeam = false;
+            else
+                participants.add(input);
+        }
+
+        FileWriter aFileWriter = new FileWriter("participants.txt");
+        PrintWriter out = new PrintWriter(aFileWriter);
+
+        for(int i = 0; i < participants.size(); i++)
+            out.println((i + 1) + "," + participants.get(i));
+            
+        aFileWriter.close();
+        out.close();
+    }
 }
