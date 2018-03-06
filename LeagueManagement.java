01454db26191d87814ceb4675857b1d3a26487b3
NameJeff <37027239+CommanderDolphin@users.noreply.github.com>
2018-03-06 20:22:28 +0000

    Update LeagueManagement.java
    -Pattern description adjusted
    -Delete League method pushed down and blanked
    -getAdminLeagues method alongside current getAdminLeagues method added
    -Provisonal editLeague method with a admin league array
    -League table generation method added
    -imported javax.swing.table.DefaultTableModel; from javax swing directory for league table

--- LeagueManagement.java
+++ LeagueManagement.java
@@ -1,6 +1,7 @@
 /**
  * LeagueManagement
  */
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JOptionPane;
 import java.io.*;
 import java.util.*;
@@ -282,46 +283,140 @@ public class LeagueManagement
 		aFileWriter.close();
 	}
 	
-	//Mitch,general plaintext string check against pattern
-	public static boolean stringCheck(String input) 
-	{
-		String pattern = "[a-z A-Z]{1,20}"; 
-		return(input.matches(pattern));
-	}
+	//Mitch,string check against pattern
+    public static boolean stringCheck(String input) 
+    {
+        String pattern = "[a-z A-Z]{1,20}"; 
+        return(input.matches(pattern));
+    }
+
+    //Mitch,number against pattern
+    public static boolean integerCheck(int input) 
+    {
+        String resultInt = ""+input;
+        String pattern = "[0-9]{1,500}"; 
+        return(resultInt.matches(pattern));
+    }
+
+    //Mitch,general password check against pattern 
+    public static boolean passwordCheck(String input) 
+    {
+        String resultPassword = ""+input;
+        String pattern = "[A-Za-Z0-9]{5,30}"; 
+        return(resultPassword.matches(pattern));
+    }
 	
-	//Mitch,password against pattern
-	public static boolean integerCheck(int input) 
-	{
-		String resultInt = ""+input;
-		String pattern = "[0-9]{1,500}"; 
-		return(resultInt.matches(pattern));
-	}
-	
-	//Mitch,general number check against pattern 
-	public static boolean passwordCheck(String input) 
-	{
-		String resultPassword = ""+input;
-		String pattern = "[A-Za-Z0-9]{5,30}"; 
-		return(resultPassword.matches(pattern));
-	}
-	
-	//Mitch,general delete league method 
-	public static void deleteLeague(File admin,String username) throws IOException
-	{
-		String blank = "";
-		FileReader aFileReader = new FileReader("leagues.txt");
-		Scanner in = new Scanner(aFileReader);
-		FileWriter aFileWriter = new FileWriter("leagues.txt");
-		PrintWriter out = new PrintWriter(aFileWriter);
-		String leagueLine;
-		int idNum = findIdentifierNumber(admin,username.toString());
-		while(in.hasNext()) //for(int i = 0; i < lineNumber && in.hasNext(); i++)
-		{
-				
-		}
+	//Mitch,//identify league admin
+	public static void getAdminLeagues() throws IOException 
+        FileReader aFileReader = new FileReader("leagues.txt");
+        Scanner in = new Scanner(aFileReader);
+        //int idNum = findIdentifierNumber(admin,username.toString());
+		int i = 0;
+        while(in.hasNextLine())
+        {   
+            String data = in.nextLine();
+            if(data.contains(","))
+            { 
+                leagueAdminNum=(data.substring((data.lastIndexOf(",")+1)));
+				if(usernameID ==  Integer.parseInt(leagueAdminNum))
+				{
+					leagueInsert=(data.substring((data.indexOf(",")+1), data.lastIndexOf(","))); 
+					tableChoices[i]=leagueInsert;
+					i++;
+				}
+            }
+        }   
 		in.close();
-		aFileReader.close();
-		out.close();
-		aFileWriter.close();
-	}		
+        aFileReader.close();
+        out.close();
+        aFileWriter.close();
+	}
+	/**
+	\\MITCH// FOR LATER
+	public static void getAdminLeagues() throws IOException
+    {
+        ArrayList<ArrayList<String>> leagues = new ArrayList<ArrayList<String>>();
+        String filename = "leagues.txt";
+        String fileError = filename + " not found";
+        String leagueError = "No leagues created";
+        File leaguesFile = new File(filename);
+        String leagueElements[];
+        int    leagueAdminID, adminID;
+        boolean found = false;
+        if (!(leaguesFile.exists()))
+        {
+            System.out.println(fileError);
+        }
+        else if (leaguesFile.length() == 0)
+        {
+            System.out.println(leagueError);
+        }
+        else
+        {
+            Scanner in = new Scanner(leaguesFile);
+            leagues.add(new ArrayList<String>());
+            leagues.add(new ArrayList<String>());
+            leagues.add(new ArrayList<String>());
+            while (in.hasNext())
+            {
+                leagueElements = (in.nextLine()).split(",");
+                for (int i = 0; i < leagueElements.length; i++)
+                {
+                    (leagues.get(i)).add(leagueElements[i]);
+                }
+            }
+            in.close();
+            for (int list = 0; list < leagues.size(); list++)
+            {
+                for (int item = 0; item < leagues.get(list).size(); item++)
+                {
+                    System.out.println(leagues.get(list).get(item));
+                }
+            }
+        }
+    }
+	**/
+
+    //Mitch,sort leagues for respective admin
+    public static void editLeague() throws IOException //identify league
+    {
+        String input = (String) JOptionPane.showInputDialog(null, "Choose a league to manage:",
+        "League Management",JOptionPane.QUESTION_MESSAGE, null, tableChoices,tableChoices[1]); 
+        in.close();	
+    }
+
+	//Mitch,general delete league method 
+    public static void deleteLeague() throws IOException //identify league
+    {
+        
+    }
+	
+	//Mitch,Generation of League Table
+	public static void generateLeagueTable()
+    {
+        String[][] leagueTable = new String[][]{
+                {"1","leagues.get(i)","x","x","x","x","x"},
+                {"2","leagues.get(i)","x","x","x","x","x"},
+                {"3","leagues.get(i)","x","x","x","x","x"},
+                {"4","leagues.get(i)","x","x","x","x","x"},
+                {"5","leagues.get(i)","x","x","x","x","x"},
+                {"6","leagues.get(i)","x","x","x","x","x"},
+                {"7","leagues.get(i)","x","x","x","x","x"},
+                {"8","leagues.get(i)","x","x","x","x","x"}};
+        String[] columnNames = {"Position","Team","Games","Wins","Draws","Losses","Score"};
+        String[][] data = new String[leagueTable.length][leagueTable[0].length];
+        for (int i = 0; i < data.length; i++)
+        {
+            for (int j = 0; j < data[i].length; j++)
+            {
+                data[i][j] = leagueTable[i][j];
+            }
+        }
+		//--Important table stuff
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        JTable table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+		//--Important table stuff
+        JOptionPane.showMessageDialog(null, scrollPane, "Output",JOptionPane.INFORMATION_MESSAGE);
+    }
 }
