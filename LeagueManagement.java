d88a73a5419627bd078f45bf14620eb5cb52bac3
RJOC <34495523+RJOC@users.noreply.github.com>
2018-03-20 23:59:37 +0000

    Update LeagueManagement.java

--- LeagueManagement.java
+++ LeagueManagement.java
@@ -1,15 +1,26 @@
 public class LeagueManagement
 {
-  public static void main(String args[]) throws IOException
-  {
-   
-   
-   
-    String blueJ = blueJ;
-  if(blueJ == blueJ)
-  {
-    System.out.println("blueJ");
-    System.out.println("nameJeff");
-  }
-  }
+ //METHOD NEEDED FOR LUKE, PLEASE REFERENCE IF NEEDS BE CHEERS 
+  public static int LeagueNumberFromName ( String leagueName)  throws IOException
+{
+	FileReader aFileReader = new FileReader("leagues.txt");
+		Scanner in = new Scanner (aFileReader) ;
+		String leagueN, leagueInfo;
+		int leagueNumber=0;
+		while(in.hasNext())
+		{
+			leagueInfo= in.nextLine();
+			String leagueInfoArr[] = leagueInfo.split(",");
+			leagueN = leagueInfoArr[1];
+			
+			if (leagueN.equals(leagueName))
+			{
+				String leagueNum = leagueInfoArr[0] ;
+				leagueNumber = Integer.parseInt(leagueNum);
+			}
+		
+			
+		}
+		return leagueNumber;
+}
 }
